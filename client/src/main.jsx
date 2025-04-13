6622ffcb647b07f4e07043ee0c14bab051127ea9
Dipak-Dhariyaparmar <dipakdhariyaparmar@gmail.com>
2025-04-13 12:44:21 +0530

    refactor: standardize HTML doctype and improve formatting in index.html; clean up imports and comments in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './route/index'
-import { Provider } from 'react-redux'
-import { store } from './store/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./route/index";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider store={store}>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </Provider>
   // </StrictMode>,
-)
+);
+//redux toolkit is added
+//react router dom is added
+//react hot toast is added
+//react icons is added
+//react query is added
+//react query devtools is added
