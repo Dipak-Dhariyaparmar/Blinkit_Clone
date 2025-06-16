0715810a1a20f5e07f565f0ccefde81a711f5ead
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-06-16 21:29:59 +0530

    Update category.route.js

--- server/route/category.route.js
+++ server/route/category.route.js
@@ -15,3 +15,4 @@ categoryRouter.put("/update", auth, updateCategoryController);
 categoryRouter.delete("/delete", auth, deleteCategoryController);
 
 export default categoryRouter;
+// This Routes is a related to category
