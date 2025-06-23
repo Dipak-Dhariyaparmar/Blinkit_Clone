1f98b52f3c0115a1407f5b53f7538abb26f069cf
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-06-23 22:07:18 +0530

    Update subCategory.model.js

--- server/models/subCategory.model.js
+++ server/models/subCategory.model.js
@@ -25,4 +25,3 @@ const subCategorySchema = new mongoose.Schema(
 const SubCategoryModel = mongoose.model("subCategory", subCategorySchema);
 
 export default SubCategoryModel;
-// This model is a related to Subcategory
