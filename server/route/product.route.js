4dbd249b795678fdda9c2ba6094c967cc5164bcc
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-06-29 11:30:38 +0530

    Update product.route.js

--- server/route/product.route.js
+++ server/route/product.route.js
@@ -33,3 +33,4 @@ productRouter.delete("/delete-product", auth, admin, deleteProductDetails);
 productRouter.post("/search-product", searchProduct);
 
 export default productRouter;
+// This route is related product 
