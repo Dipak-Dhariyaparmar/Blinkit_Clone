aca307f40b758eae54b480a86712f647bf898834
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-08-18 13:23:59 +0530

    Update generatedOtp.js

--- server/utils/generatedOtp.js
+++ server/utils/generatedOtp.js
@@ -2,4 +2,4 @@ const generatedOtp = () => {
   return Math.floor(Math.random() * 900000) + 100000; /// 100000 to 999999
 };
 export default generatedOtp;
-// This is a generated otp related utils
+// This utils is a related to generate otp
