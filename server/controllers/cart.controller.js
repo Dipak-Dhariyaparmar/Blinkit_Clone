c7dcadd8ccdf0e8f295230bc8a156181c8d34af8
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-11-24 21:01:58 +0530

    Update cart.controller.js

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,4 +1,4 @@
-// This controller is related to the carts
+// This controller is related to the cart
 
 import CartProductModel from "../models/cartproduct.model.js";
 import UserModel from "../models/user.model.js";
